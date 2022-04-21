8925d8b982f54d0f8e1af9307dcfd18e22379ef4
Sandhyakalaichelvan <102655180+Sandhyakalaichelvan@users.noreply.github.com>
2022-04-21 12:37:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+
+let n=0
+    let num1=30,num2=10,num3=15,num4=20,num5=10;
+    var total=0
+    function button1(){
+        if(n<1)     
+        {  
+            n++
+        
+            total += num1
+           
+        
+        
+        console.log("t1",total)
+       
+        var b = document.getElementById("pay");
+        b.innerHTML=total;
+        
+        
+        
+       
+    let a = document.createElement('p1')
+    a.innerHTML='Patty...30'
+    document.getElementById('fieldset').appendChild(a)
+
+
+   
+    let aa = document.getElementById('burger1')
+    aa.style.backgroundColor='brown'
+    aa.style.height='20px'
+    aa.style.width= '1oopx'
+    aa.style.borderRadius ='4px'
+    aa.style.margin = 'auto'
+    document.getElementById('div300').appendChild(aa)
+}}
+
+    
+let j=0
+function button2(){
+
+    if(j<1)
+    {
+        j++
+    
+        total += num2
+    
+    console.log("t2",total)
+   
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+
+  
+    let b = document.createElement('p')
+    b.innerHTML='Cheese...10'
+    document.getElementById('fieldset').appendChild(b)
+
+   
+    let bb = document.getElementById('burger2')
+    bb.style.backgroundColor='orange'
+    bb.style.height='15px'
+    bb.style.width= '1oopx'
+    bb.style.borderRadius ='4px'
+    bb.style.margin = 'auto'
+    document.getElementById('div300').appendChild(bb)
+}
+}
+let k=0
+function button3(){
+    if(k<1)
+    {
+        k++
+    
+        total += num3
+    
+    console.log("t3",total)
+   
+    var d = document.getElementById("pay");
+    d.innerHTML=total;
+   
+    let c = document.createElement('p')
+    c.innerHTML='Tomatoes.....15'
+    document.getElementById('fieldset').appendChild(c)
+    
+    let cc = document.getElementById('burger3')
+    cc.style.backgroundColor='red'
+    cc.style.height='10px'
+    cc.style.width= '1oopx'
+    cc.style.borderRadius ='4px'
+    cc.style.margin = 'auto'
+    document.getElementById('div300').appendChild(cc)
+}
+}
+let l=0
+function button4(){
+    if(l<1){
+        l++
+   
+        total += num4
+    
+    console.log("t4",total)
+    
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+    let d = document.createElement('p')
+    d.innerHTML='Onion....20' 
+    document.getElementById('fieldset').appendChild(d)
+   
+    let dd = document.getElementById('burger4')
+    dd.style.backgroundColor='pink'
+    dd.style.height='15px'
+    dd.style.width= '1oopx'
+    dd.style.borderRadius ='4px'
+    dd.style.margin = 'auto'
+    document.getElementById('div300').appendChild(dd)
+}
+}
+let m=0
+function button5(){
+    if(m<1){
+        m++
+    
+    
+        total += num5
+    
+    console.log("t5",total)
+    
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+   
+    let e = document.createElement('p')
+    e.innerHTML='Lettuce.....10'
+    document.getElementById('fieldset').appendChild(e)
+  
+     let dd = document.getElementById('burger5')
+    dd.style.backgroundColor='green'
+    dd.style.height='8px'
+    dd.style.width= '1oopx'
+    dd.style.borderRadius ='4px'
+    dd.style.margin = 'auto'
+    document.getElementById('div300').appendChild(dd)
+}
+}
+
+let ff = document.getElementById('burger0')
+ff.style.backgroundColor='yellow'
+ff.style.height='30px'
+ff.style.width= '1oopx'
+ff.style.borderRadius ='4px'
+ff.style.borderBottomRightRadius='30px'
+ff.style.borderBottomLeftRadius='30px'
+    document.getElementById('div200').appendChild(ff)
+
+    let gg = document.getElementById('burger6')
+gg.style.backgroundColor='yellow'
+gg.style.height='60px'
+gg.style.width= '1oopx'
+gg.style.borderRadius ='4px'
+gg.style.borderTopRightRadius='90px'
+gg.style.borderTopLeftRadius='90px'
+    document.getElementById('div100').appendChild(gg)
